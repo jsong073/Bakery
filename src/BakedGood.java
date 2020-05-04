e44c9ffd810bab969c1f9702912789d6158e3263
jsong073 <jaehasong073@gmail.com>
2020-05-04 14:49:47 -0400

    push by Jaeha

--- src/BakedGood.java
+++ src/BakedGood.java
@@ -1,4 +1,84 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class BakedGood {
+    private String name;
+    private double price;
+    private String expirationDate;
+    private ArrayList<String> dietRestriction;
+    private int quantity;
+
+    //constructors
+    public BakedGood () {       //default constructor
+        name = "";
+        price = 0.0;
+        expirationDate = "";
+        dietRestriction = new ArrayList<>();
+        quantity = 0;
+    }
+
+    public BakedGood (String name, double price, String expirationDate, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.expirationDate = expirationDate;
+        dietRestriction = new ArrayList<String>();
+        this.quantity = quantity;
+    }
+
+    //getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public ArrayList<String> getDietRestriction() {
+        return dietRestriction;
+    }
+
+    public void setDietRestriction(ArrayList<String> dietRestriction) {
+        this.dietRestriction = dietRestriction;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    //Method
+    @Override
+    public String toString() {
+        String information = "";
+        information += "Name: " + this.name + "\n";
+        information += "Price: " + this.price + "\n";
+        information += "Expiration Date: " + expirationDate + "\n";
+        information += "Diet Restrictions: " + dietRestriction + "\n";
+        information += "Quantity: " + quantity;
+        return information;
+    }
+
+    public void addDietRestriction(String ingredient) {
+        dietRestriction.add(ingredient);
+
+    }
 }
--- src/BakeryApp.java
+++ src/BakeryApp.java
@@ -2,6 +2,15 @@ import java.util.*;
 
 public class BakeryApp {
     public static void main(String[] args) {
-        
+        BakedGood example = new BakedGood();
+        BakedGood example2 = new BakedGood("cake", 25, "March 9 2020", 5);
+        BakedGood cookie = new BakedGood("cookie", 5, "5/21/2020", 20);
+        System.out.println(example2.getName());
+        System.out.println(example2.getPrice());
+        String information = example2.toString();
+        System.out.println(information);
+        System.out.println();
+        cookie.addDietRestriction("peanuts");
+        System.out.println(cookie.toString());
     }
 }
